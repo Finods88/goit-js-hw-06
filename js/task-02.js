1acb624c7db7070642c25e477ec5574d065309de
Alexander <finods@gmail.com>
2023-11-29 19:36:20 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,15 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-const ingredientsList = document.getElementById("ingredients");
+
+const fragment = document.createDocumentFragment();
 
 ingredients.forEach((ingredient) => {
   const liElement = document.createElement("li");
   liElement.classList.add("item");
   liElement.textContent = ingredient;
-  ingredientsList.appendChild(liElement);
+  fragment.appendChild(liElement);
 });
+
+const ingredientsList = document.getElementById("ingredients");
+ingredientsList.appendChild(fragment);
